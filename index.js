40445c90deada5cdf0278791042dbb4e23a330b1
EMMA KAREN <mumokaren26@gmail.com>
2024-10-02 22:21:36 +0300

    add file

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++){
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+    }
+    wrapGifts(gifts);
+    
+    function writeCards(names){
+    const messages = [];
+    for (let a = 0; a < names.length; a++){
+    messages.push(`Thank you, ${names[a]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+    }
+    
+    function countDown(number){
+    for (let b = number; b >= 0; b--){
+    console.log(b);
+    } 
+    }
+    
